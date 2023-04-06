e118c69a56c72b9e64d329706119567a7e8dfb8f
hridoy007-coder <hridoykarmokar882@gmail.com>
2023-04-06 23:29:15 +0600

    implement delete form cart

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import './Orders.css'
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
-    const cart = useLoaderData();
-    console.log(cart);
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart);
+    // console.log(savedCart);
+
+    const handelRemoveFromCart = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
 
     return (
         <div className='shop-container'>
-            <div className='products-container'>
-                <h2>Order page: {cart.length}</h2>
+            <div className='review-container'>
+                {/* <h2>Order page: {savedCart.length}</h2> */}
+                {
+                    savedCart.map(product => <ReviewItem
+                        key={product.id}
+                        product={product}
+                        handelRemoveFromCart={handelRemoveFromCart}
+                    ></ReviewItem>)
+                }
             </div>
             <div className='cart-container'>
                 <Cart cart={cart}></Cart>
