b23e4f7dbeab5c85493de13f4c78ec637c30f669
hridoy007-coder <hridoykarmokar882@gmail.com>
2023-04-06 21:15:46 +0600

    card set in order page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -27,8 +27,7 @@ const Cart = ({ cart }) => {
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Swlwcted Items: {quantity}</p>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price:$ {totalPrice}</p>
             <p>Total Shipping:$ {totalShipping}</p>
             <p>Tax:$ {tax.toFixed(2)}</p>
